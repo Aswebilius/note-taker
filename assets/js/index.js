17f76d033861c379abde346bb213c08ed59b3530
Aswebilius <81036333+Aswebilius@users.noreply.github.com>
2021-07-08 18:16:11 -0400

    revised code

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,13 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
+const express = require("express");
+const fs = require("fs");
+const path =require("path");
+const notes = require("./db/db.json");
+const uuid = require("uuid");
+const app = express();
+var port = process.env.PORT || 3017;
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
@@ -181,3 +188,16 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
+
+var port = process.env.PORT || 3017;
+
+//Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.stactic("./public"));
+
+
+//Get API db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(_dirname, "./db/db.json"))
+});
